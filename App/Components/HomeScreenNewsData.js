ec9ddab2f668ec8b322fbc34aff33dbb73b5d8b6
Veiler <linhnv110196@gmail.com>
2020-07-06 13:35:09 +0700

    Add style cho News

--- App/Components/HomeScreenNewsData.js
+++ App/Components/HomeScreenNewsData.js
@@ -2,25 +2,25 @@ const newsData = [
     {
         key: "news1",
         header: "This is Header",
-        desc: "Ahiho",
+        desc: "Descriptions",
         content: "This is News"
     },
     {
         key: "news2",
         header: "This is Header",
-        desc: "Ahiho",
+        desc: "Descriptions",
         content: "This is News"
     },
     {
         key: "news3",
         header: "This is Header",
-        desc: "Ahiho",
+        desc: "Descriptions",
         content: "This is News"
     },
     {
         key: "news4",
         header: "This is Header",
-        desc: "Ahiho",
+        desc: "Descriptions",
         content: "This is News"
     }
 ];
