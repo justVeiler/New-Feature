91ac63336cbbf04e35950ee9a77de7ec54c8ee4e
Veiler <linhnv110196@gmail.com>
2020-07-06 22:37:29 +0700

    chuyen code ve camera, chinh app.js

--- App/Components/Camera.js
+++ App/Components/Camera.js
@@ -8,42 +8,42 @@ import {
 } from "react-native";
 import { RNCamera } from "react-native-camera";
 
-class ExampleApp extends PureComponent {
+export default class ExampleApp extends PureComponent {
   render() {
     return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: "Permission to use camera",
-            message: "We need your permission to use your camera",
-            buttonPositive: "Ok",
-            buttonNegative: "Cancel"
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: "Permission to use audio recording",
-            message: "We need your permission to use your audio",
-            buttonPositive: "Ok",
-            buttonNegative: "Cancel"
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
-        />
-        <View
-          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}>
-          <TouchableOpacity
-            onPress={this.takePicture.bind(this)}
-            style={styles.capture}>
-            <Text style={{ fontSize: 14 }}> SNAP </Text>
-          </TouchableOpacity>
+        <View style={styles.container}>
+          <RNCamera
+              ref={ref => {
+                this.camera = ref;
+              }}
+              style={styles.preview}
+              type={RNCamera.Constants.Type.back}
+              flashMode={RNCamera.Constants.FlashMode.on}
+              androidCameraPermissionOptions={{
+                title: "Permission to use camera",
+                message: "We need your permission to use your camera",
+                buttonPositive: "Ok",
+                buttonNegative: "Cancel"
+              }}
+              androidRecordAudioPermissionOptions={{
+                title: "Permission to use audio recording",
+                message: "We need your permission to use your audio",
+                buttonPositive: "Ok",
+                buttonNegative: "Cancel"
+              }}
+              onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                console.log(barcodes);
+              }}
+          />
+          <View
+              style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}>
+            <TouchableOpacity
+                onPress={this.takePicture.bind(this)}
+                style={styles.capture}>
+              <Text style={{ fontSize: 14 }}> SNAP </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
     );
   }
 
