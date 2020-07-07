e5796286aeeb8c8b11eb80bc1a83188fc57be2c8
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-07-07 15:49:45 +0700

    AddButtonCamera screen is null

--- App/Components/AddButtonCamera.js
+++ App/Components/AddButtonCamera.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {View, Text, TouchableHighlight, Image} from 'react-native';
+import {View, Text, TouchableHighlight, Image, TouchableOpacity} from 'react-native';
 import styles from './Styles/AddButtonCameraStyle';
 import images from '../Images/images';
 
-export default function AddButtonCamera(props) {
+export default function AddButtonCamera() {
   return (
     <View style={styles.container}>
       <View style={styles.button}>
-        <TouchableHighlight underlayColor={'#7f58ff'}>
+        <TouchableOpacity underlayColor={'#7f58ff'} >
           <View>
             <Image source={images.plus} />
           </View>
-        </TouchableHighlight>
+        </TouchableOpacity>
       </View>
     </View>
   );
--- App/Navigation/BottomNavigation.js
+++ App/Navigation/BottomNavigation.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../Containers/HomeScreen";
 import AddButtonCamera from '../Components/AddButtonCamera';
 import { Image, Text, View, StyleSheet } from "react-native";
 import images from "../Images/images";
 
-const Tab = createBottomTabNavigator();
+export const Tab = createBottomTabNavigator();
 
 function Account() {
   return (
@@ -29,12 +28,17 @@ export default function BottomNavigator() {
           }}
         />
         <Tab.Screen
-          Screen={null}
           name={"Camera"}
           component={AddButtonCamera}
           options={{
+            unmountOnBlur: false,
             tabBarLabel: "",
             tabBarIcon: ({}) => <AddButtonCamera />
+          }}
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault(); // — > the main part
+            },
           }}
         />
 
