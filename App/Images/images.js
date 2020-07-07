46bf1ae5ee27d46782cacc8a674822533619e7f4
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-07-07 16:32:28 +0700

    tang kich thuoc image plus

--- App/Images/images.js
+++ App/Images/images.js
@@ -1,11 +1,11 @@
 const images = {
   background: require("./Background/background.jpg"),
   iconHome: require("./Icons/home.png"),
-  plus: require('./Icons/bt.png'),
+  plus: require('./Icons/bt2x.png'),
   iconAccount: require("./Icons/account.png"),
   slide1: require("../Images/Slider/silde1.jpg"),
   slide2: require("../Images/Slider/slide2.jpg"),
   slide3: require("../Images/Slider/silde1.jpg"),
-  avatar: require("../Images/Pictures/user.png")
+  // avatar: require("../Images/Pictures/user.png")
 };
 export default images;
