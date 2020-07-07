0f1d4cbcba5934cd3aab496955c5023c45e98d14
Veiler <linhnv110196@gmail.com>
2020-07-07 15:03:13 +0700

    add account page

--- App/Components/AddButtonCamera.js
+++ App/Components/AddButtonCamera.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {View, Text, TouchableHighlight, Image} from 'react-native';
 import styles from './Styles/AddButtonCameraStyle';
 import images from '../Images/images';
+import {navigate} from '../Navigation/RootNavigation';
 
-export default function AddButtonCamera(props) {
+export default function AddButtonCamera() {
   return (
     <View style={styles.container}>
       <View style={styles.button}>
--- App/Containers/AccountScreen.js
+++ App/Containers/AccountScreen.js
@@ -16,32 +16,20 @@ export default function AccountScreen() {
     <ImageBackground source={images.background} style={styles.backgroundImage}>
       <SafeAreaView style={{ flex: 1 }}>
         <StatusBar barStyle={"dark-content"} translucent={true} />
-        <View style={{ flexDirection: "row",
-            alignItems: "center", }}>
-          <View
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            source={images.avatar}
+            resizeMode={"cover"}
+            resizeMethod={"resize"}
             style={{
-              width: (100 / 414) * Dimensions.get("screen").width,
-              height: (100 / 736) * Dimensions.get("screen").height,
+              width: 120,
+              height: 120,
               borderRadius: 60,
-              backgroundColor: "golden",
-              justifyContent: "center",
-              alignItems: "center",
-                marginLeft: 20,
-                marginTop : 10
-            }}>
-            <Image
-              source={images.avatar}
-              resizeMode={"cover"}
-              resizeMethod={"resize"}
-              style={{
-                width: 110,
-                height: 110,
-                  borderRadius: 60,
-
-              }}
-            />
-          </View>
-          <Text style={{marginLeft : 16}}>Hi There</Text>
+              marginLeft: 16,
+              marginTop: 12
+            }}
+          />
+            <Text style={{ marginLeft: 16 }}>Hi There</Text>
         </View>
       </SafeAreaView>
     </ImageBackground>
