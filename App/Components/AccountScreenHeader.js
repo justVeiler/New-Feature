d2004a0a00d8290dd7fe4096a8fe42887cfc75d2
Veiler <linhnv110196@gmail.com>
2020-07-07 17:28:41 +0700

    Merge Button Add Cam 1.1.3

--- App/Components/AccountScreenHeader.js
+++ App/Components/AccountScreenHeader.js
@@ -19,12 +19,12 @@ export default function AccountScreenHeader() {
         <SafeAreaView style={{ flex: 1 }}>
           <StatusBar barStyle={"dark-content"} translucent={true} />
           <View style={{ flexDirection: "row", alignItems: "center" }}>
-            {/*<Image*/}
-            {/*  source={images.avatar}*/}
-            {/*  resizeMode={"cover"}*/}
-            {/*  resizeMethod={"resize"}*/}
-            {/*  style={styles.avatarStyle}*/}
-            {/*/>*/}
+            <Image
+              source={images.avatar}
+              resizeMode={"cover"}
+              resizeMethod={"resize"}
+              style={styles.avatarStyle}
+            />
             <Text style={{ marginLeft: 16 }}>Hi There</Text>
           </View>
         </SafeAreaView>
