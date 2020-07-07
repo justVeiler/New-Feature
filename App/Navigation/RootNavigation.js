090437a52c6ae11ea04e13a2a0319e18c22d1511
Veiler <linhnv110196@gmail.com>
2020-07-07 14:12:08 +0700

    add disable yellowbox warning

--- App/Navigation/RootNavigation.js
+++ App/Navigation/RootNavigation.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 export const navigationRef = React.createRef();
-
+console.disableYellowBox = true;
 export function navigate(name, params = {}) {
   navigationRef.current?.navigate(name, params);
 }
