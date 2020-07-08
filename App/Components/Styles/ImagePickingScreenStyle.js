3f7e01025611c0007057433f32b06d289e7e5115
Veiler <linhnv110196@gmail.com>
2020-07-08 18:09:47 +0700

    add button Back

--- App/Components/Styles/ImagePickingScreenStyle.js
+++ App/Components/Styles/ImagePickingScreenStyle.js
@@ -4,7 +4,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     marginTop: 40
   },
   image: {
--- App/Containers/ImagePickingScreen.js
+++ App/Containers/ImagePickingScreen.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
-import ImagePicker from "react-native-image-picker";
-import { navigate } from "../Navigation/RootNavigation";
+import { Image, Text, View } from "react-native";
 import styles from "../Components/Styles/ImagePickingScreenStyle";
+import GobackButton from "../Components/GobackButton";
 
 export default class ImagePickingScreen extends React.Component {
   constructor(props) {
@@ -17,6 +16,7 @@ export default class ImagePickingScreen extends React.Component {
     const { params } = route;
     return (
       <View style={styles.container}>
+        <GobackButton />
         <View style={styles.container}>
           <Image source={{ uri: params.uri }} style={styles.image} />
           <Text style={styles.sourceText}>{"Source: " + route.params.uri}</Text>
