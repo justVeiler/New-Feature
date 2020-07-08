562abe2c448aa1a5666d621a0b41dc1d016da1a4
Veiler <linhnv110196@gmail.com>
2020-07-08 17:11:31 +0700

    Done Button Camera + fix styles

---
+++ App/Components/Styles/ImagePickingScreenStyle.js
@@ -0,0 +1,24 @@
+import { Dimensions, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 40
+  },
+  image: {
+    marginLeft: 16,
+    marginRight: 16,
+    width: (382 / 414) * Dimensions.get("screen").width,
+    height: (400 / 736) * Dimensions.get("screen").height
+  },
+  sourceText: {
+    marginLeft: 16,
+    alignItems: "center",
+    marginRight: 16,
+    marginTop: 10
+  }
+});
+
+export default styles;
