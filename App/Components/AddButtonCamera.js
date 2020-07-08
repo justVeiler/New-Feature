c9a68e7c3e2506454c0e28ab36966f78e969dfed
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-07-08 09:42:01 +0700

    doi button

--- App/Components/AddButtonCamera.js
+++ App/Components/AddButtonCamera.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View, TouchableHighlight, Image, TouchableOpacity, Animated} from 'react-native';
+import {navigate} from '../Navigation/RootNavigation';
 import styles from './Styles/AddButtonCameraStyle';
 import images from '../Images/images';
 
@@ -7,6 +8,7 @@ export default function AddButtonCamera(){
   const buttonSize = new Animated.Value(1);
   const mode = new Animated.Value(0);
   function handlePress() {
+    // navigate()
     Animated.sequence([
       Animated.timing(buttonSize,{
         toValue: 0.95,
@@ -23,47 +25,16 @@ export default function AddButtonCamera(){
   const sizeStyle = {
     transform: [{scale: buttonSize}]
   }
-  const rotation = mode.interpolate({
-    inputRange: [0,1],
-    outputRange: ['0deg', '90deg']
-  })
-  const thermometerX = mode.interpolate({
-    inputRange: [0, 1],
-    outputRange: [5, -50]
-  });
-  const thermometerY = mode.interpolate({
-    inputRange: [0, 1],
-    outputRange: [5, -40]
-  });
-  const timeX = mode.interpolate({
-    inputRange: [0, 1],
-    outputRange: [10, 65]
-  });
-  const timeY = mode.interpolate({
-    inputRange: [0, 1],
-    outputRange: [2, -40]
-  });
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{position: 'absolute', left: thermometerX, top: thermometerY}}>
-        <TouchableOpacity>
-        <View style={styles.secondaryButton}>
-          <Image source={images.plus} />
-        </View>
-        </TouchableOpacity>
-      </Animated.View>
-      <Animated.View style={{position: 'absolute', left: timeX, top: timeY}}>
-        <View style={styles.secondaryButton}>
-          <Image source={images.plus} />
-        </View>
-      </Animated.View>
+
       <Animated.View style={[styles.button, sizeStyle]}>
-        <TouchableHighlight underlayColor={'#7f58ff'} onPress={handlePress}>
-          <Animated.View style={{transform: [{rotate: rotation}]}}>
+        <TouchableOpacity underlayColor={'#7f58ff'} onPress={handlePress} >
+        {/*<Animated.View style={{transform: [{rotate: rotation}]}}>*/}
             <Image source={images.plus} />
-          </Animated.View>
-        </TouchableHighlight>
+          {/*</Animated.View>*/}
+        </TouchableOpacity>
       </Animated.View>
     </View>
   );
