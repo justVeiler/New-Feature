41d8146491c3cbc40fb118f880b3194cb48e09ed
quandepzai <43404519+quandepzai@users.noreply.github.com>
2020-07-09 10:12:17 +0700

    color

--- App/Components/Styles/HomeScreenHeaderStyle.js
+++ App/Components/Styles/HomeScreenHeaderStyle.js
@@ -1,16 +1,16 @@
 import { StyleSheet, Dimensions } from "react-native";
-import Colors from 'react-native/Libraries/NewAppScreen/components/Colors';
+import Colors from '../../Themes/Colors';
 
 const styles = StyleSheet.create({
   backgroundImage: {
     justifyContent: 'center',
     alignItems: 'center',
     width: "100%",
-    height: (160 / 736) * Dimensions.get("screen").height
+    height: (160 / 736) * Dimensions.get("screen").height,
   },
   text: {
     fontSize: 30,
-    color: Colors.paleGrey
+    color: Colors.paleGrey,
   }
 });
 export default styles;
