7166e66a22c11f5dbe89aea953bd7410340206cb
Veiler <linhnv110196@gmail.com>
2020-07-09 17:22:48 +0700

    fix style Detail + Home screen

--- App/Containers/DetailScreen.js
+++ App/Containers/DetailScreen.js
@@ -6,19 +6,20 @@ import styles from "../Components/Styles/HomeScreenNewsStyle";
 export default function DetailScreen(props) {
   const { route } = props;
   const { params } = route;
+  console.log("PAAAAA", params);
   return (
-      <View style={{ flex: 1, marginLeft: 16, marginRight: 16, marginTop: 40 }}>
-        <FlatList
-          data={newsData}
-          renderItem={() => (
-            <View>
-              <Text style={styles.headerText}>{params.item.header}</Text>
-              <Text style={styles.descText}>{params.item.desc}</Text>
-              <Text style={styles.contentText}>{params.item.content}</Text>
-            </View>
-          )}
-          style={{ marginTop: 5 }}
-        />
-      </View>
+    <View style={{ flex: 1, marginLeft: 16, marginRight: 16, marginTop: 40 }}>
+      <FlatList
+        data={[params]}
+        renderItem={() => (
+          <View>
+            <Text style={styles.headerText}>{params.item.header}</Text>
+            <Text style={styles.descText}>{params.item.desc}</Text>
+            <Text style={styles.contentText}>{params.item.content}</Text>
+          </View>
+        )}
+        style={{ marginTop: 5 }}
+      />
+    </View>
   );
 }
