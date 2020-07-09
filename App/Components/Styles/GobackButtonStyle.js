8e07ce4225b13a1d3dbb86da1d85113c27485e99
Veiler <linhnv110196@gmail.com>
2020-07-09 11:12:55 +0700

    fix style goback Button

--- App/Components/Styles/GobackButtonStyle.js
+++ App/Components/Styles/GobackButtonStyle.js
@@ -4,15 +4,24 @@ import colors from "../../Themes/Colors";
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    alignItems: "flex-start",
-    padding: 5,
-    marginLeft: 11
+    padding: 10,
+    marginLeft: 10,
+    marginTop: (15 / 736) * Dimensions.get("screen").height,
+    backgroundColor: "white",
+    marginRight: (320 / 414) * Dimensions.get("screen").width
   },
   arrow: {
     height: (18 / 736) * Dimensions.get("screen").height,
-    width: (12 / 414) * Dimensions.get("screen").width
+    width: (12 / 414) * Dimensions.get("screen").width,
+    opacity: 0.7
   },
-  text: { marginLeft: 3, color: colors.blueGrey }
+  text: {
+    fontSize: (16 / 736) * Dimensions.get("screen").height,
+    lineHeight: (18 / 736) * Dimensions.get("screen").height,
+    marginLeft: 3,
+    color: colors.blueGrey,
+    flex: 1
+  }
 });
 
 export default styles;
--- App/Components/Styles/ImagePickingScreenStyle.js
+++ App/Components/Styles/ImagePickingScreenStyle.js
@@ -4,13 +4,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    marginTop: 40
+    marginTop: 20
   },
   image: {
     marginLeft: 16,
     marginRight: 16,
     width: (382 / 414) * Dimensions.get("screen").width,
-    height: (400 / 736) * Dimensions.get("screen").height
+    height: (450 / 736) * Dimensions.get("screen").height
   },
   sourceText: {
     marginLeft: 16,
