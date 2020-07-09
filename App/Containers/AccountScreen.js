b73c35a65e5bfec0108f35b3d44a9d84c7f8a48a
Veiler <linhnv110196@gmail.com>
2020-07-09 16:20:54 +0700

    fix style account screen

--- App/Containers/AccountScreen.js
+++ App/Containers/AccountScreen.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { View } from "react-native";
+import { ScrollView } from "react-native";
 import AccountScreenInfoList from "../Components/AccountScreenInfoList";
 import AccountScreenHeader from "../Components/AccountScreenHeader";
 
 export default function AccountScreen() {
   return (
-    <View style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1 }}>
       <AccountScreenHeader />
       <AccountScreenInfoList />
-    </View>
+    </ScrollView>
   );
 }
