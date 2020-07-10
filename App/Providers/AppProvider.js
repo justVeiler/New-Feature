85119f66c3e40b4e7e56d3231c84d7b740ff55df
Veiler <linhnv110196@gmail.com>
2020-07-10 14:37:33 +0700

    Add action, reducer va provider de lam image history

--- App/Providers/AppProvider.js
+++ App/Providers/AppProvider.js
@@ -1,12 +1,12 @@
-import initialState, { AppReducer } from "../ReduxHooks/AppReducers";
+import initialState, { AppReducers } from "../ReduxHooks/AppReducers";
 import React, { createContext, useReducer } from "react";
-import {AppActions, AuthActions} from '../ReduxHooks/AppActions';
+import { AppActions } from "../ReduxHooks/AppActions";
 
 export const AppContext = createContext({});
 export const AppProvider = AppContext.Provider;
 
 export default function Wrapper(props) {
-  const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(AppReducers, initialState);
   const actions = mapActionsToDispatch(dispatch);
 
   return (
@@ -17,9 +17,9 @@ export default function Wrapper(props) {
 }
 
 const uploadHistory = dispatch => async (): void => {
-  const { data } = response;
+  const { data } = await response;
   console.log(data);
-  dispatch({type:AppActions.uploadHistory, payload : imageSource})
+  dispatch({ type: AppActions.uploadHistory });
 };
 
 const mapActionsToDispatch = dispatch => {
