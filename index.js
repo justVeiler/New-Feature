9d494738d86fd1a98d3f6e32b31d4d52298ded95
Veiler <linhnv110196@gmail.com>
2020-07-10 14:07:43 +0700

    remove express + formidable

--- index.js
+++ index.js
@@ -5,9 +5,6 @@
 import { AppRegistry } from "react-native";
 import App from "./App/Containers/App";
 import { name as appName } from "./app.json";
-let express = require("express");
-let app = express();
-let formidable = require('express')
-
 
 AppRegistry.registerComponent(appName, () => App);
+
