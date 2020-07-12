b9062e615521bffd982adf1c69a13af2cba1a3d5
Veiler <linhnv110196@gmail.com>
2020-07-12 17:15:12 +0700

    add async storage save items

--- App/Containers/ImagePickingScreen.js
+++ App/Containers/ImagePickingScreen.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import styles from "../Components/Styles/ImagePickingScreenStyle";
 import GobackButton from "../Components/GobackButton";
-import { navigate } from "../Navigation/RootNavigation";
+import AsyncStorage from "@react-native-community/async-storage";
 
 export default function ImagePickingScreen(props) {
   const { route } = props;
   const { params } = route;
+  const { uri } = params;
   const onPress = () => {
-    navigate("AccountScreen", {
-      uri: params.uri
-    });
+    try {
+      (async function() {
+        await AsyncStorage.setItem("URI", uri);
+      })();
+    } catch (e) {}
   };
 
   return (
@@ -18,9 +21,7 @@ export default function ImagePickingScreen(props) {
       <GobackButton />
       <View style={styles.container}>
         <Image source={{ uri: params.uri }} style={styles.image} />
-        <TouchableOpacity
-          style={styles.buttonUpload}
-          onPress={() => onPress()}>
+        <TouchableOpacity style={styles.buttonUpload} onPress={() => onPress()}>
           <Text>UPLOAD</Text>
         </TouchableOpacity>
       </View>
