b64aee5cea81196f64702bd5d5bf24782aeb7f3f
Veiler <linhnv110196@gmail.com>
2020-07-14 10:10:37 +0700

    Add ListHeader Footer, replace code, reformat code

--- App/Containers/AccountScreen.js
+++ App/Containers/AccountScreen.js
@@ -1,13 +1,30 @@
-import React from "react";
-import { ScrollView } from "react-native";
-import AccountScreenInfoList from "../Components/AccountScreenInfoList";
+import React, { useContext } from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import AccountScreenHeader from "../Components/AccountScreenHeader";
+import { AppContext } from "../Providers/AppProvider";
+import styles from "../Components/Styles/AccountScreenInfoListStyle";
 
 export default function AccountScreen() {
+  const appContext = useContext(AppContext);
+  const { images } = appContext.state;
+  console.log("DATA", images);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={{ flexDirection: "column" }}>
+        <Text style={{ marginLeft: 16, fontSize: 20 }}>Date</Text>
+        <TouchableOpacity style={{ padding: 5 }}>
+          <Image source={{ uri: item }} style={styles.imageStyle} />
+        </TouchableOpacity>
+      </View>
+    );
+  };
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <AccountScreenHeader />
-      <AccountScreenInfoList />
-    </ScrollView>
+    <FlatList
+      style={{ flex: 1 }}
+      ListHeaderComponent={<AccountScreenHeader />}
+      data={images}
+      renderItem={renderItem}>
+    </FlatList>
   );
 }
